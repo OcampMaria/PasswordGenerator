a18a58f4895fa8c46e72177b5ebf1befbeb8422a
Maria Ocampo <mariaocampo0513@gmail.com>
2020-10-24 16:26:52 -0700

    edited variables for character types

--- Develop/script.js
+++ Develop/script.js
@@ -3,29 +3,32 @@
 
 // // WHEN I click the button to generate a password
 // // THEN I am presented with a series of prompts for password criteria
-//   *********************prompt with criterias
+
 // // WHEN prompted for password criteria
 // // THEN I select which criteria to include in the password
-//   ****************select which criteria I want? to include in my password
+
 // // WHEN prompted for the length of the password
 // // THEN I choose a length of at least 8 characters and no more than 128 characters
-//  *************** criteria of length for PW 
+
 // // WHEN prompted for character types to include in the password
 // // THEN I choose lowercase, uppercase, numeric, and/or special characters
-//   ******************criteria for characters
+
 // // WHEN I answer each prompt
 // // THEN my input should be validated and at least one character type should be selected
-//  ********* validate each criteria after completed. a character type selected? 
+
 // // WHEN all prompts are answered
 // // THEN a password is generated that matches the selected criteria
-//  *********** generate password after all criteria is completed. 
+ 
 // // WHEN the password is generated
 // // THEN the password is either displayed in an alert or written to the page
-//  ************ display in alert!! 
 
 
-  var characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$*".split("")
-
+// set variables for character types: lowercase, uppercase, numeric, and/or special characters
+  var specialChar = "!@#$%^&*".split("");
+  var numeric = "1234567890".split("")
+  var loweCase = "abcdefghijklmnopqrstuvwxyz".split("");
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -36,8 +39,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  
 
 }
 
